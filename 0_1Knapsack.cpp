9ed8eac92403e5949241188cffba7be50f1446b4
isha0612 <77613722+isha0612@users.noreply.github.com>
2022-06-18 15:32:43 +0530

    Update 0_1Knapsack.cpp

--- 0_1Knapsack.cpp
+++ 0_1Knapsack.cpp
@@ -1,3 +1,4 @@
+// Top-Down
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -25,4 +26,34 @@ int main() {
     int weight; cin>>weight;
     int ans = 0;
     cout<<knapsack(k, weight, n, dp);
+}
+
+// Bottom-Up
+#include <bits/stdc++.h>
+using namespace std; 
+
+int main() {
+    vector<pair<int, int>> k;
+    int n; cin>>n;
+    for(int i = 0; i < n; i++) {
+        int w, v;
+        cin>>w>>v;
+        k.push_back({w, v});
+    }
+    int w; cin>>w;
+    int dp[n + 1][w + 1];
+    for(int i = 0; i <= n; i++) {
+        for(int j = 0; j <= w; j++) {
+            if(i == 0 || j == 0) {
+                dp[i][j] = 0;
+            }
+            else if(k[i - 1].first > j) {
+                dp[i][j] = dp[i - 1][j];
+            }
+            else {
+                dp[i][j] = max(dp[i - 1][j], k[i - 1].second + dp[i - 1][j - k[i - 1].first]);
+            }
+        }
+    }
+    cout<<dp[n][w]<<endl;
 }
