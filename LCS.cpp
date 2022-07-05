c1762e2dbb6564facc8a7160b6830bdff9c85bc9
isha0612 <77613722+isha0612@users.noreply.github.com>
2022-07-05 23:29:37 +0530

    Update LCS.cpp

--- LCS.cpp
+++ LCS.cpp
@@ -1,3 +1,4 @@
+//Bottom - Up
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -15,4 +16,27 @@ int main() {
     string a, b;
     cin>>a>>b;
     return lcs(a, b, a.size(), b.size());
+}
+
+//Top - Down
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string a, b;
+    cin>>a>>b;
+    int m = a.size(), n = b.size();
+    int dp[m + 1][n + 1];
+    for(int i = 0; i <= m; i++) {
+        for(int j = 0; j <= n; j++) {
+            if(i == 0 || j == 0)
+                dp[i][j] = 0;
+            else if(a[i - 1] == b[j - 1]) 
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            else {
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+    cout<<dp[m][n];
 }
